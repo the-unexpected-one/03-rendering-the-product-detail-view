5f5529db668d7d64b33ac53d0a54c1e7c67014ee
Saloni <saloni.patil2220@gmail.com>
2022-09-01 13:04:47 +0530

    Making SQL Easy With Sequelize

--- util/database.js
+++ util/database.js
@@ -1,10 +1,9 @@
-const mysql= require('mysql2')
-
-//instead of a collection we create a collection pool. This alows to always reach out to it whenever we have a query to run and we can manage multiple queries simultaneously as each query need sits own connection
-const pool=mysql.createPool({
-    host: 'localhost',
-    user:'root',
-    database: 'node-complete',
-    password:'Saloni@22'
+//SEquelize uses SQL behind the scenes
+const Sequelize=require('sequelize');
+ 
+const sequelize=new Sequelize('node-complete','root','Saloni@22',{
+    dialect: 'mysql',
+    host:'localhost'
 });
-module.exports=pool.promise();
+
+module.exports=sequelize;
